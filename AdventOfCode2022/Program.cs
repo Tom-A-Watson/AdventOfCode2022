91970bacb306bc15c72bff1aba628aff3927e994
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2023-04-26 16:40:14 +0100

    Attempted to re-solve challenge 1 part 1 using C#. Getting FileNotFoundException

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,7 +1,33 @@
-﻿public class Program
+﻿int currentElf = 1;
+int maxElf = 1;
+int currentCalories = 0;
+int maxCalories = 0;
+
+string line;
+
+using (StreamReader reader = new("day1.txt"))
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+	while ((line = reader.ReadLine()) != null)
+	{
+		if (string.IsNullOrWhiteSpace(line))
+		{
+			// Start a new elf's inventory
+			currentElf++;
+			currentCalories = 0;
+		}
+		else 
+		{
+			// Add the number of calories to the current elf's inventory
+			currentCalories = int.Parse(line);
+
+			// Updates the elf with the most calories
+			if (currentCalories > maxCalories) 
+			{
+				maxElf = currentElf;
+				maxCalories = currentCalories;
+			}
+		}
+	}
 }
+
+Console.WriteLine("Elf {0} has the most calories with a total of: {1}", maxElf, maxCalories);
